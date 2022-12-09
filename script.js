3f178cc12f529a5111ae117d1b528c9b338a2610
nebu <nebu.nebuchadnezzar@gmail.com>
2022-12-08 19:15:55 -0500

    stuff

---
+++ script.js
@@ -0,0 +1,15 @@
+class Header extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <nav>
+      <div class="navbar">
+        <a href="/index.html" class="underline">home</a>
+        <a href="/writing.html" class="underline">writing</a>
+        <a href="/about.html" class="underline">about</a>
+      </div>
+    </nav>
+    `;
+  }
+}
+
+customElements.define('main-header', Header);
